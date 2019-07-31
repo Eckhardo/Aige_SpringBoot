84d46ce7d04e030b7683dc2dd0331e3ea669434c
eckhardo <1af112AB>
2019-07-31 09:40:42 +0200

    Add BEANR

--- src/test/java/com/eki/TestData.java
+++ src/test/java/com/eki/TestData.java
@@ -5,5 +5,6 @@ public class TestData {
 	public static final String LOCATION_HKHKG = "HKHKG";
 	public static final String LOCATION_COCTG = "COCTG";
 	public static final String LOCATION_ARBUE = "ARBUE";
-        public static final String LOCATION_DEHAM = "DEHAM";
+    public static final String LOCATION_DEHAM = "DEHAM";
+    public static final String LOCATION_BEANR = "BEANR";
 }
